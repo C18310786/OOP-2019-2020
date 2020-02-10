0a67bf737205e1aa8c2b152d12fa5bc07eb043ea
Roberto <robdmc34@gmail.com>
2020-02-10 12:59:16 +0000

    Lab3

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -23,6 +23,24 @@ public class Loops extends PApplet
 
 	public void draw()
 	{	
-		background(0);		
+		background(0);	
+
+		stroke(255, 255, 255);
+		line(10, 10, 100, 10);
+		line(10, 20, 100, 20);
+		line(10, 30, 100, 30);
+		line(10, 40, 100, 40);
+
+		for(int y = 10; y <= 40; y += 10)
+		{
+			line(200, y, 300, y);
+		}
+
+		int yy = 10;
+		while(yy <= 40)
+		{
+			line(300, yy, 400, yy);
+			yy += 10;
+		}
 	}
 }
